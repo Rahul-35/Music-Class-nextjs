05ac368a0bd29aabf3621a5349e126a957b34649
Rahul Ch <xavi3441@gmail.com>
2024-10-05 14:24:38 +0530

    Added Testimonial+Upcoming Webinar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import Hero from "@/components/HeroSection";
+import TestiCards from "@/components/TestimonialCards";
+import UpWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return(
-    <main className="min-h-screen bg-black/[0.94]  antialiased bg-grid-white/[0.2]">
+    <main className="min-h-screen bg-black/[0.94]  antialiased ">
       <Hero/>
       <FeaturedCourses/>
       <WhyChooseUs/>
+      <TestiCards/>
+      <UpWebinar/>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
-
+const svgToDataUri = require("mini-svg-data-uri");
 // const defaultTheme = require("tailwindcss/defaultTheme");
-// const colors = require("tailwindcss/colors");
+//const colors = require("tailwindcss/colors");
 //const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
 
 
@@ -11,7 +11,7 @@ import type { Config } from "tailwindcss";
 function addVariablesForColors({ addBase, theme }: any) {
   const allColors = theme('colors');
   const newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
 
   addBase({
@@ -19,6 +19,28 @@ function addVariablesForColors({ addBase, theme }: any) {
   });
 }
 
+function addSvgPattern ({ matchUtilities, theme }: any) {
+  matchUtilities(
+    {
+      "bg-grid": (value: any) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+        )}")`,
+      }),
+      "bg-grid-small": (value: any) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+        )}")`,
+      }),
+      "bg-dot": (value: any) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+        )}")`,
+      }),
+    },
+    { values: theme("backgroundColor"), type: "color" }
+  );
+}
 
 
 const config: Config = {
@@ -52,6 +74,6 @@ const config: Config = {
       }
     },
   },
-  plugins: [addVariablesForColors],
+  plugins: [addVariablesForColors, addSvgPattern],
 };
 export default config;
